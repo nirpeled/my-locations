5521c24b7f643362c39bc14b6c2484d7bf39136b
Nir Peled <nir@wisdo.com>
2016-07-09 09:22:28 +0300

    [FIX] show stats on homepage

--- app/containers/homepage/homepage-container.js
+++ app/containers/homepage/homepage-container.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
 import classNames from 'classnames';
 import helpers from '../../helpers/helpers.js';
 
@@ -12,16 +13,17 @@ var HomepageContainer = React.createClass({
 
         helpers.logger('[HomepageContainer] render');
 
+        var props = this.props,
+            categories = _.get(props, 'categories.items', {}),
+            locations = _.get(props, 'locations.items', {});
+
         return (
             <section className="box-row box-homepage">
 
                 <ul className="table">
 
-                    {
-                        _.times(1, (index) => {
-                            return <li key={index}>Homepage</li>
-                        })
-                    }
+                    <li>Categories ({_.size(categories)})</li>
+                    <li>Locations ({_.size(locations)})</li>
 
                 </ul>
                 
@@ -30,4 +32,11 @@ var HomepageContainer = React.createClass({
     }
 });
 
-export default HomepageContainer;
+function mapStateToProps(state) {
+    return {
+        categories: _.get(state, 'categories'),
+        locations: _.get(state, 'locations')
+    };
+}
+
+export default connect(mapStateToProps)(HomepageContainer);
